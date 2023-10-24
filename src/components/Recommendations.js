34714919ed3a5f3fcfd00844b88b9ea32b9f579c
Mohammad-AbuJaber <mohammadabujaber2001@gmail.com>
2023-10-25 00:41:06 +0300

    Adding page component

--- src/components/Recommendations.js
+++ src/components/Recommendations.js
@@ -2,6 +2,7 @@ import React from "react";
 import '../stylesheets/Recommendations.css';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faSearch} from '@fortawesome/free-solid-svg-icons';
+import {faCheckCircle} from '@fortawesome/free-regular-svg-icons';
 
 function SearchBar() {
     return (
@@ -26,11 +27,45 @@ function Card(props) {
     );
 }
 
+export function Page(props) {
+    return (
+        <div className="page">
+            <div className="avatar">
+                <img src={props.avatarUrl} alt="Avatar"/>
+            </div>
+            <div className="info">
+                <h2 className="name">{props.name} {props.isVerified &&
+                    <FontAwesomeIcon icon={faCheckCircle} className="verified-badge"/>}</h2>
+                <p className="username">@{props.username}</p>
+            </div>
+            <button className="follow-button">Follow</button>
+        </div>
+    );
+}
+
 export default function Recommendations() {
     return (
         <div className="recommendations">
             <SearchBar/>
             <Card title="You might like">
+                <Page
+                    name="Tailwind CSS"
+                    username={"tailwindcss"}
+                    avatarUrl="https://asset.brandfetch.io/idKJ12s-EY/idI9erPtdw.jpeg"
+                    isVerified={true}
+                />
+                <Page
+                    name="TypeScript"
+                    username={"typescript"}
+                    avatarUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1024px-Typescript_logo_2020.svg.png"
+                    isVerified={false}
+                />
+                <Page
+                    name="freeCodeCamp.org"
+                    username={"freeCodeCamp"}
+                    avatarUrl="https://pbs.twimg.com/profile_images/1276770212927410176/qTgTIejk_400x400.jpg"
+                    isVerified={true}
+                />
             </Card>
         </div>
     );
